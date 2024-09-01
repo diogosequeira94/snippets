5b17e4debed3572a146658a394e3e9d312b1c8e0
diogosequeira94 <diogosequeira@hotmail.fr>
2024-09-01 13:14:16 +0100

    add custom not-found page and custom loading page

--- src/app/snippets/[id]/page.tsx
+++ src/app/snippets/[id]/page.tsx
@@ -1,4 +1,4 @@
-import { notFound } from 'next/navigation';
+import { notFound } from "next/navigation";
 import { db } from "@/db";
 
 interface SnippetShowPageProps {
@@ -8,11 +8,13 @@ interface SnippetShowPageProps {
 }
 
 export default async function SnippetShowPage(props: SnippetShowPageProps) {
+  await new Promise((r) => setTimeout(r, 2000));
+
   const snippet = await db.snippet.findFirst({
     where: { id: parseInt(props.params.id) },
   });
 
-  if(!snippet){
+  if (!snippet) {
     return notFound();
   }
 
