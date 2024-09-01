c804f1805208bd493c3a044ec312fda4573173dc
diogosequeira94 <diogosequeira@hotmail.fr>
2024-09-01 16:12:28 +0100

    add snippets on home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,29 @@
+import Link from "next/link";
 import { db } from "@/db";
 
 export default async function Home() {
-  const snippes = await db.snippet.findMany();
+  const snippets = await db.snippet.findMany();
 
-  const renderedSnippets = snippes.map((snippet) => {
-    return <div key={snippet.id}>{snippet.title}</div>;
+  const renderedSnippets = snippets.map((snippet) => {
+    return (
+      <Link
+        href={`/snippets/${snippet.id}`}
+        key={snippet.id}
+        className="flex justify-between items-center p-2 border rounded"
+      >
+        <div>{snippet.title}</div>
+        <div>View</div>
+      </Link>
+    );
   });
 
   return (
     <div>
-     {renderedSnippets}
+      <div className="flex m-2 justify-between items-center">
+        <h1 className="text-2xl font-bold">Snippets</h1>
+        <Link href='snippets/new' className="border p-2 rounded">New</Link>
+      </div>
+      <div className="flex flex-col gap-2">{renderedSnippets}</div>
     </div>
   );
 }
