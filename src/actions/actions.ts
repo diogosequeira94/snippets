4274205a65490ee993534238558d3fe8e38d2ed8
diogosequeira94 <diogosequeira@hotmail.fr>
2024-09-02 20:18:55 +0100

    finished implementing action for editting snippets

--- src/actions/actions.ts
+++ src/actions/actions.ts
@@ -1,10 +1,13 @@
-'use server';
+"use server";
 
-import { db } from '@/db';
+import { redirect } from "next/navigation";
+import { db } from "@/db";
 
-export async function editSnippet(id: number, code: string){
-    await db.snippet.update({
-        where: { id },
-        data: { code }
-    });
+export async function editSnippet(id: number, code: string) {
+  await db.snippet.update({
+    where: { id },
+    data: { code },
+  });
+
+  redirect(`/snippets/${id}`);
 }
